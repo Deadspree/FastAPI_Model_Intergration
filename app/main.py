e8e1b8aa9f3010cd19b4338e7b96161b7d1230dc
Deadspree <minhc613@gmail.com>
2024-10-11 21:54:42 +0900

    SIUU

---
+++ app/main.py
@@ -0,0 +1,47 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from fastapi import File, UploadFile
+from PIL import Image
+from io import BytesIO
+from app.model import DogCatModel
+from torchvision import transforms
+import torch
+import torchvision.models as models
+import logging
+import app.config.logging_config as config
+app = FastAPI()
+config.initLog()
+log = logging.getLogger("dog-cat")
+def read_imagefile(data) -> Image.Image:
+    image = Image.open(BytesIO(data))
+    return image
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/pet/")
+async def predict(file: UploadFile=File(...)):
+    image = read_imagefile(await file.read())
+    tfms= transforms.Compose([
+                                            transforms.Resize((224,224)),
+                                            transforms.RandomRotation(20),
+                                            transforms.RandomVerticalFlip(p=0.1),
+                                            transforms.ToTensor(),
+                                            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+                                            ])
+    image = tfms(image)
+    image = image[None,:]
+    dcModel = DogCatModel(models.resnet34(pretrained=True))
+    #dcModel.load_state_dict(torch.load('C:/Users/Admin_PC/fastapi-dog-cat-main/model_weights.pth',map_location=torch.device('cpu')))
+    dcModel.eval()
+    with torch.no_grad():
+        prediction= dcModel(image)
+        val,label = torch.max(prediction,dim=1)
+        log.info(f"Prediction: {label.item()} with val {val.item()}")
+        if val > 0.7:
+            label = label.item()
+            return {"prediction": label}
+        else:
+            return {"prediction": "Unknown"}
